487869525d102caccb552957390bb1da4f64195d
akshat0047 <pande.akshat21@gmail.com>
2018-08-01 01:51:14 +0530

    smooth scroll added

--- js/custom.js
+++ js/custom.js
@@ -91,6 +91,7 @@ $(document).ready(function()
 
 	/*
 
+
 	3. Init Menu
 
 	*/
@@ -112,7 +113,6 @@ $(document).ready(function()
 			});
 		}
 	}
-
 	function openMenu()
 	{
 		menu.addClass('active');
@@ -360,7 +360,35 @@ $(document).ready(function()
 			});
 		}
 	}
+
 });
+$(document).ready(function(){
+  // Add smooth scrolling to all links
+  $(".smooth-scroll").on('click', function(event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top -2
+      }, 700, function(){
+
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+				
+      });
+	}// End if
+  });
+});
+
 	function project_button(){
 		$(".projects").toggle();
 	}
